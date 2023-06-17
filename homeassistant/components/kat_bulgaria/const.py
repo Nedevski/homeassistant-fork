951a7ccd96b73c2e342a26c858e5b1dbc5ed5220
Nikola Nedevski <nikola.nedevski@gmail.com>
2023-06-17 11:18:56 +0000

    Fixed some PR comments

--- homeassistant/components/kat_bulgaria/const.py
+++ homeassistant/components/kat_bulgaria/const.py
@@ -6,6 +6,4 @@ CONF_PERSON_EGN = "egn"
 CONF_DRIVING_LICENSE = "driving_license_number"
 CONF_PERSON_NAME = "person_name"
 
-BINARY_SENSOR_NAME_PREFIX = "Globi "
-
 ATTR_LAST_UPDATED = "last_updated"
