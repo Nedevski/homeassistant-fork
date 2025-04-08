cf146721c972f4e41634b7cb9677d66485e77809
Nikola Nedevski <nikola.nedevski@gmail.com>
2025-04-08 10:20:47 +0000

    Updated initial flow step

--- homeassistant/components/kat_bulgaria/config_flow.py
+++ homeassistant/components/kat_bulgaria/config_flow.py
@@ -27,17 +27,6 @@ from .kat_client import KatClient
 
 _LOGGER = logging.getLogger(__name__)
 
-SCHEMA_START = vol.Schema(
-    {
-        vol.Required(CONF_PERSON_TYPE, default=PersonType.INDIVIDUAL): SelectSelector(
-            SelectSelectorConfig(
-                options=[PersonType.INDIVIDUAL, PersonType.BUSINESS],
-                translation_key=CONF_PERSON_TYPE,
-            )
-        )
-    }
-)
-
 SCHEMA_INDIVIDUAL = vol.Schema(
     {
         vol.Required(CONF_PERSON_NAME): str,
@@ -80,27 +69,11 @@ class ConfigFlowHandler(ConfigFlow, domain=DOMAIN):
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
-        """Handle the initial step."""
+        """User config step to choose if setting up for an individual or a business."""
 
-        # If no input, show default form
-        if user_input is None:
-            return self.async_show_form(step_id=STEP_ID_USER, data_schema=SCHEMA_START)
-
-        person_type = user_input[CONF_PERSON_TYPE]
-
-        if person_type == PersonType.INDIVIDUAL:
-            return await self.async_step_individual(user_input)
-        if person_type == PersonType.BUSINESS:
-            return await self.async_step_business(user_input)
-
-        # If we reach here, something went wrong.
-        _LOGGER.error("Invalid person type: %s", person_type)
-
-        # Show the form again with an error
-        return self.async_show_form(
-            step_id=STEP_ID_USER,
-            data_schema=SCHEMA_START,
-            errors={"base": "invalid_type"},
+        return self.async_show_menu(
+            step_id="user",
+            menu_options=[STEP_ID_INDIVIDUAL, STEP_ID_BUSINESS],
         )
 
     async def async_step_individual(
@@ -108,7 +81,7 @@ class ConfigFlowHandler(ConfigFlow, domain=DOMAIN):
     ) -> ConfigFlowResult:
         """Handle the initial step."""
 
-        if len(user_input) == 1:
+        if user_input is None:
             return self.async_show_form(
                 step_id=STEP_ID_INDIVIDUAL, data_schema=SCHEMA_INDIVIDUAL
             )
@@ -118,6 +91,7 @@ class ConfigFlowHandler(ConfigFlow, domain=DOMAIN):
         user_egn = user_input[CONF_PERSON_EGN]
         user_document_type = user_input[CONF_DOCUMENT_TYPE]
         user_license_number = user_input[CONF_DOCUMENT_NUMBER]
+
         user_input[CONF_PERSON_TYPE] = PersonType.INDIVIDUAL
 
         kat_client = KatClient(
@@ -147,7 +121,7 @@ class ConfigFlowHandler(ConfigFlow, domain=DOMAIN):
     async def async_step_business(self, user_input: dict[str, Any]) -> ConfigFlowResult:
         """Handle the initial step."""
 
-        if len(user_input) == 1:
+        if user_input is None:
             return self.async_show_form(
                 step_id=STEP_ID_BUSINESS, data_schema=SCHEMA_BUSINESS
             )
@@ -157,6 +131,7 @@ class ConfigFlowHandler(ConfigFlow, domain=DOMAIN):
         user_egn = user_input[CONF_PERSON_EGN]
         user_gov_id_number = user_input[CONF_DOCUMENT_NUMBER]
         user_bulstat = user_input[CONF_BULSTAT]
+
         user_input[CONF_PERSON_TYPE] = PersonType.BUSINESS
 
         kat_client = KatClient(
